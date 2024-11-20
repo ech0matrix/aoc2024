da9e7db7b76244f2b1ec80f30b7d837bad93359c
Daniel Marcotte <daniel.p.marcotte@gmail.com>
2024-11-20 14:58:36 -0600

    Day00: Initial setup, and copy over 2023 utils

---
+++ src/Day00.kt
@@ -0,0 +1,16 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readInput("Day00_test")
+    check(part1(testInput) == 1)
+
+    val input = readInput("Day00")
+    println(part1(input))
+    println(part2(input))
+}
